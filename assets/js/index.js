d41f63c5347820fc5453d65a1b2fbb953fa30856
Sang <dotiensang123@gmail.com>
2022-09-02 14:52:10 +0700

    not responsive yet

--- assets/js/index.js
+++ assets/js/index.js
@@ -84,15 +84,11 @@ $(document).ready(function menuMobile() {
 }
 )
 
-function map() {
+$(document).ready(function map() {
     /**
    * ---------------------------------------
    * This demo was created using amCharts 4.
-   *
-   * For more information visit:
    * https://www.amcharts.com/
-   *
-   * Documentation is available at:
    * https://www.amcharts.com/docs/v4/
    * ---------------------------------------
    */
@@ -124,9 +120,47 @@ function map() {
     // Create hover state and set alternative fill color
     var hs = polygonTemplate.states.create("hover"); //tạo ra hover
     hs.properties.fill = am4core.color("aqua"); //thay đổi màu địa phận khi hover vào
+    //chart homeZoom và home GeoPoint là sẽ zoom đến điểm chỉ định, lấy chỉ số bằng cách vào f12 console gõ chart.homeZoomLevel - chart.homeGeoPoint
+    // (Ko hiểu sao khi viết ra file riêng ko dùng 2 lệnh trên đc mà phải nhúng map trực tiếp vào html rồi nhập lệnh mới ra rồi lấy chỉ số gắn ngược lại đây)
+    chart.homeZoomLevel = 3.3;
+    chart.homeGeoPoint = {
+        latitude: 21.405135621844433,
+        longitude: 106.37697680683515
+    };
 
     // chart.events.on("ready", function (ev) { //function chạy khi bắt đầu 
-    //     chart.zoomToMapObject(polygonSeries.getPolygonById("VN-HN")); //function chạy sẽ zoom đến khu vực chỉ định theo id (xem trong file của các nước trong geodata)
+    //     // chart.zoomToMapObject(polygonSeries.getPolygonById("VN-HN")); //function chạy sẽ zoom đến khu vực chỉ định theo id (xem trong file của các nước trong geodata)
+
     // });
+
+    //Các nút home và zoom của map
+    let zoombutton = chart.zoomControl = new am4maps.ZoomControl();
+    let button = chart.chartContainer.createChild(am4core.Button); // tạo nút home để ấn quay về ban đầu khi đang zoom
+    button.padding(5, 5, 5, 5);
+    button.align = "right";
+    button.marginRight = 15;
+    // button.width = 100; set width theo px
+    // container.width = am4core.percent(100); set width theo %
+    // button.fill = am4core.color("green"); Thay đổi màu nền
+    // rect.stroke = am4core.color("red"); Thay đổi màu viền
+    button.events.on("hit", function () {
+        chart.goHome();
+    });
+    let icon_button = button.icon = new am4core.Sprite();
+    icon_button.path = "M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 10 21 L 10 14 L 14 14 L 14 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z";
+
+    // Click eventlistener
+    polygonTemplate.events.on("hit", function (ev) { //bắt sự kiện click 'hit' của amchart tạo ra function truyền vào đối số ev 
+        // function removeDiv() {
+        //     document.getElementsByClassName('ampopup-content').style.display = "none";
+        // }
+        chart.closeAllPopups(); //khi function chạy sẽ đóng tất cả những popups có trước đó rồi mở popup dưới
+        chart.openPopup( //mở popup
+            "Bạn muốn khám phá những điểm du lịch tại <strong>" + ev.target.dataItem.dataContext.name + "</strong><br><br>" +
+            "<div class='button-content'><a href='" + ev.target.dataItem.dataContext.id + "'>Đúng vậy! </a></div>"
+            // + "<a href='#' onclick='" + removeDiv() + "'>No </a>"
+        );
+    });
+    chart.chartContainer.wheelable = false; // Vô hiệu hóa zoom của bản đồ
 }
-map();
+)
